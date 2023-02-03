a606b869aad76035bdf8a89e8ccbcf2f706dbf1c
Jess <jessica.mancoo@hippodigital.co.uk>
2023-02-03 17:35:30 +0000

    version 1 branching attempt
    fix on monday please

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,24 @@
 // External dependencies
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
+router.post(
+  "/v1/contact-answer",
+  function (req, res) {
+    var Upload = req.session.data["contact"];
+    if (Upload == "contact-1") {
+      res.redirect("/v1/primary-prescription-summary");
+    }
 
-// Add your routes here - above the module.exports line
+    if (Upload == "contact-2") {
+      res.redirect("v1/secondary-prescription-summary");
+    }
+    if (Upload == "contact-3") {
+      res.redirect("v1/testpack-prescription-summary");
+    }
+  },
 
-module.exports = router;
+  // Add your routes here - above the module.exports line
+
+  (module.exports = router)
+);
