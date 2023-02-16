bddafea5fa1baada691459c973e06f134851f441
Jess <jessica.mancoo@hippodigital.co.uk>
2023-02-16 10:56:40 +0000

    v2 branching
    done

--- app/routes.js
+++ app/routes.js
@@ -18,7 +18,43 @@ router.post(
     }
   },
 
+  router.post("/v2/contact-answer", function (req, res) {
+    var Uploads = req.session.data["contact"];
+    if (Uploads == "primary-care") {
+      res.redirect("/v2/prescription-summary");
+    }
+
+    if (Uploads == "secondary-care") {
+      res.redirect("/v2/prescription-summary");
+    }
+    if (Uploads == "test-pack") {
+      res.redirect("/v2/testpack-prescription-summary");
+    }
+    if (Uploads == "presc-files") {
+      res.redirect("/v2/testpack-prescription-summary");
+    }
+  }),
+
   // Add your routes here - above the module.exports line
 
   (module.exports = router)
 );
+
+//branchinng v2
+
+//
+//router.post(
+// "/v2/contact-answer",
+//function (req, res) {
+//var Uploads = req.session.data["contact"];
+//if (Uploads == "primary care") {
+//    res.redirect("/v2/prescription-summary");
+//  }
+
+//   if (Uploads == "phone") {
+//  res.redirect("/v2/prescription-summary");
+//  }
+//  if (Uploads == "text") {
+//    res.redirect("/v2/testpack-prescription-summary");
+//  }
+//},
